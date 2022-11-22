62035aecbf44ba42d631208c11b5f368840a9f18
ramazansakin <ramazansakin63@gmail.com>
2022-11-22 16:23:58 +0300

    feat: GREEN cycle > I created a main Java class to start ScoreBoard to stream

---
+++ src/main/java/com/rsakin/sportradar/casestudy/FCLiveScoreBoardApp.java
@@ -0,0 +1,10 @@
+package com.rsakin.sportradar.casestudy;
+
+public class FCLiveScoreBoardApp {
+
+    public static void main(String[] args) {
+        ScoreBoard scoreBoard = new ScoreBoard();
+        scoreBoard.startStreaming();
+    }
+
+}
---
+++ src/main/java/com/rsakin/sportradar/casestudy/ScoreBoard.java
@@ -0,0 +1,8 @@
+package com.rsakin.sportradar.casestudy;
+
+public class ScoreBoard {
+
+    public void startStreaming() {
+        System.out.println("Live Score Board Up and Running");
+    }
+}
