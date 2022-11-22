45816eaf6b112cb266a4a67768d7a6f3d958c6b1
ramazansakin <ramazansakin63@gmail.com>
2022-11-22 16:19:38 +0300

    feat: RED cycle > first we need to create a board to stream matches, now we have nothing and we get failure on testing

---
+++ src/test/java/com/rsakin/sportradar/casestudy/FCScoreBoardTest.java
@@ -0,0 +1,51 @@
+package com.rsakin.sportradar.casestudy;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static junit.framework.TestCase.assertEquals;
+
+class FCScoreBoardTest {
+
+    // To catch the stdout and stderr messages and assert with expected ones
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private ByteArrayInputStream testIn;
+
+    private final InputStream systemIn = System.in;
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+        System.setIn(systemIn);
+    }
+
+    // provides to put input for stdin
+    private void provideInput(String data) {
+        testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+    @Test
+    void shouldScoreBoardStartStreaming() {
+        ScoreBoard scoreBoard = new ScoreBoard();
+        scoreBoard.startStreaming();
+        assertEquals("Live Score Board Up and Running\r\n", outContent.toString());
+    }
+
+}
