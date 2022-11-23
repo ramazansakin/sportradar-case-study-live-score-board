b60c71774336e2cd1f48708232c720be47b6e6f5
ramazansakin <ramazansakin63@gmail.com>
2022-11-23 09:38:54 +0300

    feat: REFACTOR > we can define ScoreBoard and initOn @BeforeEach method to remove duplicate initializations

--- src/test/java/com/rsakin/sportradar/casestudy/FCScoreBoardTest.java
+++ src/test/java/com/rsakin/sportradar/casestudy/FCScoreBoardTest.java
@@ -22,8 +22,11 @@ class FCScoreBoardTest {
     private final PrintStream originalOut = System.out;
     private final PrintStream originalErr = System.err;
 
+    private ScoreBoard scoreBoard;
+
     @BeforeEach
     public void setUpStreams() {
+        scoreBoard = ScoreBoard.getScoreBoard();
         System.setOut(new PrintStream(outContent));
         System.setErr(new PrintStream(errContent));
     }
@@ -43,7 +46,6 @@ class FCScoreBoardTest {
 
     @Test
     void shouldScoreBoardStartStreaming() {
-        ScoreBoard scoreBoard = ScoreBoard.getScoreBoard();
         provideInput("exit");
         scoreBoard.startStreaming();
         assertEquals("Live Score Board Up and Running\r\n", outContent.toString());
@@ -52,7 +54,6 @@ class FCScoreBoardTest {
     // We can start new match and add to live score board
     @Test
     void shouldAcceptStartCommandToAddNewMatchToBoard() {
-        ScoreBoard scoreBoard = ScoreBoard.getScoreBoard();
         provideInput("start Mexico Uruguay\nexit");
         scoreBoard.startStreaming();
         assertEquals("Live Score Board Up and Running\r\nMatch started [ Mexico 0 - Uruguay 0 ]\r\n", outContent.toString());
@@ -60,7 +61,6 @@ class FCScoreBoardTest {
 
     @Test
     void shouldUpdateMatchScoresOnBoard() {
-        ScoreBoard scoreBoard = ScoreBoard.getScoreBoard();
         provideInput("start Mexico Uruguay\nupdate Mexico 1 Uruguay 0\nexit");
         scoreBoard.startStreaming();
         assertEquals("Live Score Board Up and Running\r\nMatch started [ Mexico 0 - Uruguay 0 ]\r\nScore updated [ Mexico 1 - Uruguay 0 ]\r\n", outContent.toString());
@@ -69,7 +69,6 @@ class FCScoreBoardTest {
     // As an edge case, if we are trying to update a match that is not being played
     @Test
     void shouldNotUpdateMatchScoresOnBoardWithNotStartedMatch() {
-        ScoreBoard scoreBoard = ScoreBoard.getScoreBoard();
         provideInput("start Mexico Uruguay\nupdate Poland 1 Uruguay 0\nexit");
         scoreBoard.startStreaming();
         assertEquals("Live Score Board Up and Running\r\nMatch started [ Mexico 0 - Uruguay 0 ]\r\n", outContent.toString());
@@ -79,7 +78,6 @@ class FCScoreBoardTest {
     // As edge case, if we are trying to update a match with negative numbers
     @Test
     void shouldNotUpdateMatchScoresOnBoardWithNegativeNumbers() {
-        ScoreBoard scoreBoard = ScoreBoard.getScoreBoard();
         provideInput("start Turkey Germany\nupdate Turkey 0 Germany -1\nexit");
         scoreBoard.startStreaming();
         assertEquals("Live Score Board Up and Running\r\nMatch started [ Turkey 0 - Germany 0 ]\r\n", outContent.toString());
@@ -89,7 +87,6 @@ class FCScoreBoardTest {
     // edge case, if we are trying to update a match with negative numbers
     @Test
     void shouldNotStartMatchOnBoardWithNotValidTeamNames() {
-        ScoreBoard scoreBoard = ScoreBoard.getScoreBoard();
         provideInput("start Turkey XYZ\nexit");
         scoreBoard.startStreaming();
         assertEquals("Not valid team name [ XYZ ]\r\n", errContent.toString());
@@ -98,7 +95,6 @@ class FCScoreBoardTest {
     // we could finish a match and remove on the board
     @Test
     void shouldFinishMatchOnBoard() {
-        ScoreBoard scoreBoard = ScoreBoard.getScoreBoard();
         provideInput("start Turkey Germany\nupdate Turkey 0 Germany -1\nfinish Turkey\nexit");
         scoreBoard.startStreaming();
         assertEquals("Live Score Board Up and Running\r\nMatch started [ Turkey 0 - Germany 0 ]\r\n" +
@@ -108,7 +104,6 @@ class FCScoreBoardTest {
     // As a similar edge case we need to see error message when we try to finish a match that has been not started yet
     @Test
     void shouldNotFinishMatchOnBoardWithNotStarted() {
-        ScoreBoard scoreBoard = ScoreBoard.getScoreBoard();
         provideInput("finish Turkey\nexit");
         scoreBoard.startStreaming();
         assertEquals("This match is not being played at the moment!\r\n", errContent.toString());
