8bd98fa3a22104c2abd8ac8308351d67de177d44
ramazansakin <ramazansakin63@gmail.com>
2022-11-23 08:36:00 +0300

    feat: RED > As an another Edge Case, we should not update the scores with negative numbers and need to expect a error message but now there is no control and the case getting failure

--- src/test/java/com/rsakin/sportradar/casestudy/FCScoreBoardTest.java
+++ src/test/java/com/rsakin/sportradar/casestudy/FCScoreBoardTest.java
@@ -67,12 +67,22 @@ class FCScoreBoardTest {
 
     // As an edge case, if we are trying to update a match that is not being played
     @Test
-    void shouldNotUpdateMatchScoresOnBoard() {
+    void shouldNotUpdateMatchScoresOnBoardWithNotStartedMatch() {
         ScoreBoard scoreBoard = ScoreBoard.getScoreBoard();
         provideInput("start Mexico Uruguay\nupdate Poland 1 Uruguay 0\nexit");
         scoreBoard.startStreaming();
         assertEquals("Live Score Board Up and Running\r\nMatch started [ Mexico 0 - Uruguay 0 ]\r\n", outContent.toString());
         assertEquals("This match is not being played at the moment!\r\n", errContent.toString());
+    }
+
+    // As an another edge case, if we are trying to update a match with negative numbers
+    @Test
+    void shouldNotUpdateMatchScoresOnBoardWithNegativeNumbers() {
+        ScoreBoard scoreBoard = ScoreBoard.getScoreBoard();
+        provideInput("start Turkey Germany\nupdate Turkey 0 Germany -1\nexit");
+        scoreBoard.startStreaming();
+        assertEquals("Live Score Board Up and Running\r\nMatch started [ Turkey 0 - Germany 0 ]\r\n", outContent.toString());
+        assertEquals("Scores can not be negative!\r\n", errContent.toString());
     }
 
 }
